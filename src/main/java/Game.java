f6bf498b3ab856284ca63e5aa1f49087a854156e
Jackliang441 <90588930+Jackliang441@users.noreply.github.com>
2021-10-15 16:04:36 -0400

    Update Game.java

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -9,14 +9,33 @@ public class Game extends JFrame implements MouseListener{
     static int X = 1;
     static int Y = 1;
     static int Allowed = 0;
+    static int Selected = 2;
 
     public void mousePressed(MouseEvent e) {
-        X = (int)(Math.ceil((e.getX()-355)/75));
-        Y = (int)(Math.ceil((e.getY()-180)/75));
-        if (X == 1 && Y == 1){
-            Allowed = 1;
+        X = (int) (Math.ceil((e.getX() - 355) / 75));
+        Y = (int) (Math.ceil((e.getY() - 180) / 75));
+        if (X == 1 && Y == 1) {
+            if (Selected == 1){
+                Selected = 2;
+                Allowed = 0;
+                state = 0;
+            }else {
+                Selected = 0;
+                Allowed = 1;
+            }
+        } else if (X == 2 && Y == 1){
+            if (Selected == 0){
+                Selected = 2;
+                Allowed = 0;
+                state = 0;
+            }else {
+                Selected = 1;
+                Allowed = 1;
+            }
         }else {
             Allowed = 0;
+            Selected = 2;
+            state = 0;
         }
     }
 
