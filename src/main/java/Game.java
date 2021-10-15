6d901468ca0a85ca38cfe33cc61410e5c8bab596
Jackliang441 <90588930+Jackliang441@users.noreply.github.com>
2021-10-14 23:48:49 -0400

    Update Game.java

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,7 +1,42 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.math.*;
 
-public class Game {
+public class Game extends JFrame implements MouseListener{
+    static int x = 0;
+
+    public void mousePressed(MouseEvent e) {
+        x = 1;
+    }
+
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    public void mouseClicked(MouseEvent e) {
+    }
+    public void mouseExited(MouseEvent e) {
+    }
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public Game() {
+        initUI();
+    }
+
+    private void initUI() {
+        add(new UI());
+        setResizable(false);
+        pack();
+        setTitle("Course Project");
+        setLocation(150,20);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        addMouseListener(this);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
     public static void main(String[] args){
         ArrayList<Character> map = new ArrayList<Character>();
         ArrayList<Character> PlayerChars = new ArrayList<Character>();
@@ -11,6 +46,10 @@ public class Game {
         map.add(player1);
         map.add(enemy1);
         Scanner sc = new Scanner(System.in);
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Game();
+            ex.setVisible(true);
+        });
 
         while(map.contains(player1) && map.contains(enemy1)) {
             System.out.println("player character at position " + map.indexOf(player1) + ", health " + player1.getCurrHealth() + "/" + player1.getMaxHealth());
