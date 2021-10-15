94d81ed5aa04fcec6f05e8f772cf11597e96dba7
James Pang <jmelvpang@gmail.com>
2021-10-15 19:53:44 -0400

    Fixed issues with null pointer exceptions

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -95,8 +95,8 @@ public class Game extends JFrame implements MouseListener{
     
     public static void main(String[] args){
         Map map1 = new Map();
-        ArrayList<Character> PlayerChar = new ArrayList<Character>();
-        ArrayList<Character> EnemyChar = new ArrayList<Character>();
+        ArrayList<Character> PlayerChar = new ArrayList<>();
+        ArrayList<Character> EnemyChar = new ArrayList<>();
         PlayerChar player1 = new PlayerChar("Player 1");
         PlayerChar player2 = new PlayerChar("Player 2");
         EnemyChar enemy1 = new EnemyChar("Enemy 1");
@@ -127,18 +127,19 @@ public class Game extends JFrame implements MouseListener{
             while (checkActions(PlayerChar)) {
                 System.out.println("enter character name to perform attack");
                 String inputString = sc.nextLine();
-                // checks if input is a character name
-                if (isValidInput(inputString, PlayerChar)) {
-                    Character user = getCharacterByName(inputString, PlayerChar);
+                Character user = getCharacterByName(inputString, PlayerChar);
+                // checks if user is null
+                if (!(user == null)) {
                     // checks if character's action has been used
                         if (user.isActionUsed()) {
                             System.out.println("Character has already used action");
                         } else {
                             System.out.println("enter name of target");
                             String targetStr = sc.nextLine();
-                            // checks if input is an enemy name
-                            if (isValidInput(targetStr, EnemyChar)) {
-                                Character target = getCharacterByName(targetStr, EnemyChar);
+                            Character target = getCharacterByName(targetStr, EnemyChar);
+                            // checks if target it null
+                            if (!(target == null)) {
+
                                 Action.attack(user, target);
                                 if (target.getCurrHealth() <= 0) {
                                     System.out.println(target.getName() + " perished");
@@ -183,15 +184,7 @@ public class Game extends JFrame implements MouseListener{
             character.restoreAction();
         }
     }
-    public static boolean isValidInput(String input, ArrayList<Character> list){
-        for (Character character: list){
-            if (input.equals(character.getName())) {
 
-                return true;
-            }
-        }
-        return false;
-    }
 
     public static Character getCharacterByName(String name, ArrayList<Character> list){
         for (Character character: list){
