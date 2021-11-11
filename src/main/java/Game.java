33cc91a21ae9acd65c3407840d4abeece9b9fc0c
Jackliang441 <90588930+Jackliang441@users.noreply.github.com>
2021-11-10 22:59:33 -0500

    Update Game.java

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -5,69 +5,27 @@ import java.awt.*;
 
 public class Game extends JFrame implements MouseListener{
     static int state = -1;
-    static int X = 1;
-    static int Y = 1;
+    static int X = -1;
+    static int Y = -1;
     static int Allowed = 0;
     static int Selected = -1;
     static int ESelected = -1;
     static int Animation = 0;
+    static int Map = 1;
+    static ArrayList <String> Chars = new ArrayList<>();
+    static ArrayList <Integer> SelectedChars = new ArrayList<>();
+    static ArrayList <Integer> UnitLocationX = new ArrayList<>();
+    static ArrayList <Integer> UnitLocationY = new ArrayList<>();
+    static ArrayList <Integer> EnemyLocationX = new ArrayList<>();
+    static ArrayList <Integer> EnemyLocationY = new ArrayList<>();
 
     public void mousePressed(MouseEvent e) {
-        if (Animation == 0) {
-            X = (int) (Math.ceil((e.getX() - 355) / 75));
-            Y = (int) (Math.ceil((e.getY() - 180) / 75));
-            if (X == 1 && Y == 1) {
-                if (Selected != -1) {
-                    Selected = -1;
-                    Allowed = 0;
-                    state = 0;
-                } else {
-                    Selected = 0;
-                    Allowed = 1;
-                }
-            } else if (X == 2 && Y == 1) {
-                if (Selected != -1) {
-                    Selected = -1;
-                    Allowed = 0;
-                    state = 0;
-                } else {
-                    Selected = 1;
-                    Allowed = 1;
-                }
-            }else if (X == 1 && Y == 2) {
-                if (Selected != 0) {
-                    Selected = -1;
-                    ESelected = -1;
-                    Allowed = 0;
-                    state = 0;
-                } else {
-                    ESelected = 0;
-                    Allowed = 1;
-                }
-            }else if (X == 2 && Y == 2) {
-                if (Selected != 1) {
-                    Selected = -1;
-                    ESelected = -1;
-                    Allowed = 0;
-                    state = 0;
-                } else {
-                    ESelected = 1;
-                    Allowed = 1;
-                }
-            } else {
-                Allowed = 0;
-                Selected = -1;
-                state = 0;
-            }
-        }
+        X = (int) (Math.ceil((e.getX() - 205) / 75));
+        Y = (int) (Math.ceil((e.getY() + 45) / 75));
+        AllyCheck();
     }
 
     public void mouseReleased(MouseEvent e) {
-        if (Animation == 0) {
-            if (Allowed == 1) {
-                state++;
-            }
-        }
     }
 
     public void mouseClicked(MouseEvent e) {
@@ -75,6 +33,18 @@ public class Game extends JFrame implements MouseListener{
     public void mouseExited(MouseEvent e) {
     }
     public void mouseEntered(MouseEvent e) {
+    }
+
+    private void AllyCheck() {
+        Selected = -1;
+        if (Game.UnitLocationX.contains(X)) {
+            for (int i = 0; i < Game.UnitLocationX.size(); i++) {
+                if (Game.UnitLocationX.get(i) == X && Game.UnitLocationY.get(i) == Y) {
+                    Selected = i + 1;
+                    break;
+                }
+            }
+        }
     }
 
     public Game() {
@@ -106,6 +76,17 @@ public class Game extends JFrame implements MouseListener{
             JFrame ex = new Game();
             ex.setVisible(true);
         });
+        SelectedChars.add(0,1);
+        SelectedChars.add(1,2);
+        SelectedChars.add(2,3);
+        SelectedChars.add(3,4);
+        Chars.add(0, "");
+        Chars.add(1, "Marth");
+        Chars.add(2, "Hector");
+        Chars.add(3, "Irelia");
+        Chars.add(4, "Robin");
+        Chars.add(5, "Sakura");
+
 
         while(map.contains(player1) && map.contains(enemy1)) {
             System.out.println("player character at position " + map.indexOf(player1) + ", health " + player1.getCurrHealth() + "/" + player1.getMaxHealth());
