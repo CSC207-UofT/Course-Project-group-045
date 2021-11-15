796bd0cf440980a77cdcb03903d17a52ed2ad84e
Jackliang441 <90588930+Jackliang441@users.noreply.github.com>
2021-11-14 21:51:20 -0500

    Update Character.java

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,17 +1,15 @@
 public abstract class Character {
     private final String name;
 
-    private int currHealth, attack, defense, speed;
-    private final int maxHealth;
+    private int currHealth, maxHealth, attack, speed;
     private boolean actionUsed;
 
-    public Character(String name) {
+    public Character(String name, int maxHealth, int attack, int speed) {
         this.name = name;
-        maxHealth = 100;
+        this.maxHealth = maxHealth;
+        this.attack = attack;
+        this.speed = speed;
         currHealth = maxHealth;
-        attack = 10;
-        defense = 10;
-        speed = 10;
         actionUsed = false;
     }
 
@@ -27,6 +25,10 @@ public abstract class Character {
         return this.attack;
     }
 
+    public int getSpeed() {
+        return this.speed;
+    }
+
     public int getCurrHealth(){
         return this.currHealth;
     }
@@ -35,12 +37,36 @@ public abstract class Character {
         this.currHealth = this.currHealth - damage;
     }
 
-    public void attack(Character user, Character target){
+    public void increaseCurrHealth(int boost) {
+        this.currHealth += boost;
+    }
+
+    public void increaseCurrAttack(int power) {
+        this.attack += power;
+    }
+
+    public void increaseCurrSpeed(int stamina) {
+        this.speed += stamina;
+    }
+
+    public boolean isActionUsed(){
+        return actionUsed;
+    }
+
+    public void restoreAction(){
+        this.actionUsed = false;
+    }
+
+    public void useAction(){
+        this.actionUsed = true;
+    }
+
+/*    public void attack(Character user, Character target){
         target.reduceCurrHealth(user.getAttack());
         System.out.println(this.name + " attacked " + target.getName() + " for " + user.getAttack() + " damage!");
         System.out.println(target.getName() + "'s health is now " + target.getCurrHealth());
         this.actionUsed = true;
-    }
+    }*/
 
 
 }
