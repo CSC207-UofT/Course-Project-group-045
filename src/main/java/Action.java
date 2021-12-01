54a9da1ac20f109248fc14a23ddc81079e2107b0
Jackliang441 <90588930+Jackliang441@users.noreply.github.com>
2021-11-30 21:50:48 -0500

    Update Action.java

--- src/main/java/Action.java
+++ src/main/java/Action.java
@@ -1,5 +1,8 @@
+import java.util.*;
 
 public class Action {
+
+    static ArrayList <Integer> Ranges = new ArrayList<>();
 
     public static void attack(Character attacker, Character target) {
         target.reduceCurrHealth(attacker.getAttack());
@@ -12,15 +15,54 @@ public class Action {
         if (target == null){
             return false;
         }
-        if (Game.currMap.charXPosition(target) == Game.currMap.charXPosition(attacker) + 1 ||
-                Game.currMap.charXPosition(target) == Game.currMap.charXPosition(attacker) - 1){
+        if (Math.abs(Game.currMap.charXPosition(attacker) -
+                Game.currMap.charXPosition(target)) + Math.abs(Game.currMap.charYPosition(attacker)
+                - Game.currMap.charYPosition(target)) <= attacker.getRange()){
             return true;
         }
-        if (Game.currMap.charYPosition(target) == Game.currMap.charYPosition(attacker) + 1 ||
-                Game.currMap.charYPosition(target) == Game.currMap.charYPosition(attacker) - 1){
-            return true;
+        for (int i = 0 ; i < 2 ; i++){
+            if (moveable(attacker, Game.currMap.charXPosition(target) + i*2 - 1, Game.currMap.charYPosition(target))){
+                move(attacker, Game.currMap.charXPosition(target) + i*2 - 1, Game.currMap.charYPosition(target));
+                return true;
+            }
+        }
+        for (int i = 0 ; i < 2 ; i++){
+            if (moveable(attacker, Game.currMap.charXPosition(target), Game.currMap.charYPosition(target) + i*2 - 1)){
+                move(attacker, Game.currMap.charXPosition(target), Game.currMap.charYPosition(target) + i*2 - 1);
+                return true;
+            }
         }
         return false;
+    }
+
+    public static boolean inRange(Character selected){
+        for (int i = 0 ; i < 4 ; i++){
+            if (Ranges.get(i) < (selected.getSpeed() + selected.getRange())){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void distances(Character selected){
+        Ranges.clear();
+        for (int i = 0 ; i < 4 ; i++){
+            Ranges.add(Math.abs(Game.currMap.charXPosition(selected) -
+                    Game.currMap.charXPosition(Game.playerChar.get(i))) + Math.abs(Game.currMap.charYPosition(selected)
+                    - Game.currMap.charYPosition(Game.playerChar.get(i))));
+        }
+    }
+
+    public static int closest(Character selected){
+        int index = 0;
+        int value = 1000;
+        for (int i = 0 ; i < 4 ; i++){
+            if (Ranges.get(i) < value){
+                index = i;
+                value = Ranges.get(i);
+            }
+        }
+        return index;
     }
 
     public static boolean moveable(Character selected, int x , int y){
@@ -28,15 +70,79 @@ public class Action {
                 && x > 0 && y > 0 &&
                 selected.getSpeed() >= Math.abs(Game.currMap.charXPosition(selected) - x) +
                         Math.abs(Game.currMap.charYPosition(selected) - y)) {
-            return true;
+            if (Math.abs(Game.currMap.charXPosition(selected) - x) +
+                    Math.abs(Game.currMap.charYPosition(selected) - y) == 2){
+                if (range2Check(selected, x, y)){
+                    return true;
+                }
+            }else{
+                return true;
+            }
         }
-
         return false;
     }
+
+    public static boolean range2Check(Character selected, int x , int y){
+        if (Game.currMap.charXPosition(selected) == x | Game.currMap.charYPosition(selected) == y) {
+            int x1 = Game.currMap.charXPosition(selected);
+            int y1 = Game.currMap.charYPosition(selected);
+            int dx = x - Game.currMap.charXPosition(selected);
+            int dy = y - Game.currMap.charYPosition(selected);
+            if (Game.currMap.getObsclOrEnemy(x1 + dx/2, y1 + dy/2) == null){
+                return true;
+            }
+        }else {
+            if (Game.currMap.getObsclOrEnemy(Game.currMap.charXPosition(selected), y) == null |
+                    Game.currMap.getObsclOrEnemy(x, Game.currMap.charYPosition(selected)) == null){
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    public static boolean boardAttackable(Character attacker, int x, int y){
+        if ((Game.enemyChar.contains(Game.currMap.getCharByPos(x, y)) | Game.currMap.getCharByPos(x, y) == null)
+                && x <= Game.currMap.col && y <= Game.currMap.row && x > 0 && y > 0){
+            if (Math.abs(Game.currMap.charXPosition(attacker) - x) + Math.abs(Game.currMap.charYPosition(attacker)
+                    - y) <= attacker.getRange()){
+                return true;
+            }
+            for (int i = 0 ; i < 2 ; i++){
+                if (moveable(attacker, x + i*2 - 1, y)){
+                    return true;
+                }
+            }
+            for (int i = 0 ; i < 2 ; i++){
+                if (moveable(attacker, x, y + i*2 - 1)){
+                    return true;
+                }
+            }
+            return false;
+        }
+        return false;
+    }
+
+    public static boolean boardMoveable(Character selected, int x , int y){
+        if ((Game.currMap.getCharByPos(x, y) == null | Game.playerChar.contains(Game.currMap.getCharByPos(x, y))) &&
+                x <= Game.currMap.col && y <= Game.currMap.row && x > 0 && y > 0 &&
+                selected.getSpeed() >= Math.abs(Game.currMap.charXPosition(selected) - x) +
+                        Math.abs(Game.currMap.charYPosition(selected) - y)) {
+            if (Math.abs(Game.currMap.charXPosition(selected) - x) +
+                    Math.abs(Game.currMap.charYPosition(selected) - y) == 2){
+                if (range2Check(selected, x, y)){
+                    return true;
+                }
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void move(Character selected, int x, int y) {
         Game.currMap.removeChar(selected);
         Game.currMap.addChar(selected, x, y);
         selected.useAction();
     }
 }
-
