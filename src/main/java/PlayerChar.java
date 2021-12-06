95f75c9b9eabeb7f6b881ab640ac1b10feb07967
Jackliang441 <90588930+Jackliang441@users.noreply.github.com>
2021-12-06 06:07:46 -0500

    Delete PlayerChar.java

--- src/main/java/PlayerChar.java
+++
@@ -1,6 +0,0 @@
-public class PlayerChar extends Character{
-
-    public PlayerChar(String name, int maxHealth, int attack, int speed, int range){
-        super(name, maxHealth, attack, speed, range);
-    }
-}
