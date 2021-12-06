c792fb8964430257d0e99c1f5b6bb973c905768b
Jackliang441 <90588930+Jackliang441@users.noreply.github.com>
2021-12-06 06:05:21 -0500

    Add files via upload

---
+++ src/main/java/Marth.java
@@ -0,0 +1,15 @@
+public class Marth extends Character {
+    public Marth() {
+        super(new Object[]{"Marth", 90, 30, 2, 4, 1, 0});
+    }
+
+    public void ultimate(Character target) {
+        this.reset_Meter();
+        target.reduceCurrHealth(this.getAttack() + 10);
+        this.increaseCurrHealth((int)(0.5 * (this.getAttack()) + 10));
+        if (this.getCurrHealth() > this.getMaxHealth()){
+            this.reduceCurrHealth(this.getCurrHealth() - this.getMaxHealth());
+        }
+        this.useAction();
+    }
+}
